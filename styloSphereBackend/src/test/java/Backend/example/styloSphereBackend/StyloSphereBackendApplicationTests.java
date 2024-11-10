59a3560e970bcd6dfe7eeb96947e8d361670f063
muhannd2004 <muhanndsayed24@gmail.com>
2024-11-11 00:36:50 +0200

    nice work

---
+++ styloSphereBackend/src/test/java/Backend/example/styloSphereBackend/StyloSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.example.styloSphereBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyloSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
