ac3c04de45c0b7e7d19e3eba892e23cb6f624d6a
muhannd2004 <muhanndsayed24@gmail.com>
2024-11-28 00:29:05 +0200

    main page done!

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import mainPagepng from '../images/mainPageImg/mainPage.png';
+import aboutuspng from '../images/mainPageImg/aboutUs1.png';
 import '../style/mainPageStyle/MainPageStyle.css';
 
 function MainPage(){
@@ -55,6 +56,118 @@ function MainPage(){
                 </div>
                 
             </div>
+            <div className='page-main-two'>
+                <div className='page-main-two-img'>
+                    <img src={aboutuspng} />
+                </div>
+                <div className='page-main-two-right'>
+                    <div className='page-main-two-head'>
+                        <h1>
+                            Who
+                            <br />
+                            We are
+                        </h1>
+                    </div>
+                    <div className='page-main-two-txt'>
+                    <p>  
+                    Welcome to our marketing hub, where creativity meets strategy! We're
+                    <br /> 
+                    here to help your brand thrive with personalized solutions that connect,
+                    <br />
+                    engage, and inspire.
+                    </p>  
+                    </div>
+                    <div className='page-main-two-button'>
+                        <a href="aboutus.html" className="button">LEARN MORE</a>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className='page-main-three'>
+                        <div className='page-main-three-contactus'>             
+                            <div className='page-main-three-contactus-title'>
+                                <h1>Contact Us</h1>
+                            </div>
+                            <p>
+                                Interested in working together? Fill out some info and we will
+                                be in touch shortly. We can’t wait to hear from you!
+                                
+                            </p> 
+                            
+                        </div>
+
+                        <div className="page-main-three-login">
+                            <form>
+                                <label htmlFor="first-name" className="label">
+                                Name <span className="required">(required)</span>
+                                </label>
+                                <div className="name-fields">
+                                    <div>
+                                        <input
+                                        type="text"
+                                        id="first-name"
+                                        name="firstName"
+                                        placeholder="First Name"
+                                        required
+                                        />
+                                    </div>
+                                <div>
+                                    <input
+                                    type="text"
+                                    id="last-name"
+                                    name="lastName"
+                                    placeholder="Last Name"
+                                    required
+                                    />                                    
+                                </div>
+                                </div>
+                                <label htmlFor="email" className="label">
+                                Email <span className="required">(required)</span>
+                                </label>
+                                <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                required
+                                />
+
+                                <label htmlFor="message" className="label label-message">
+                                Message <span className="required">(required)</span>
+                                </label>
+                                <textarea
+                                id="message"
+                                name="message"
+                                placeholder="Message"
+                                rows="4"
+                                required
+                                ></textarea>
+                                <div className="send-button">  
+                                    <button >send</button>
+                                </div>
+                               
+                            </form>
+                        </div>
+                
+            </div>  
+                <footer className="footer">
+                    <div className="footer-left">
+                        <h2>Stylo Sohere</h2>
+                        <p>Made by <a href="#">Muhannd</a></p>
+                    </div>
+                    <div className="footer-right">
+                        <div className="footer-section">
+                            <h3>Location</h3>
+                            <p>123 Demo Street<br/>New York, NY 12345</p>
+                        </div>
+                        <div className="footer-section">
+                            <h3>Contact</h3>
+                            <p><a href="mailto:email@example.com">muhanndsayed24@gmail.com</a><br/>(555) 555-5555</p>
+                        </div>
+                    </div>
+                </footer>
+
         </div>
     );
 }
