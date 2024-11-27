549846e7da5e1d0928187aee1f60a7bc839dac30
muhannd2004 <muhanndsayed24@gmail.com>
2024-11-27 16:15:35 +0200

    some of the main done

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import mainPagepng from '../images/mainPageImg/mainPage.png';
+import '../style/mainPageStyle/MainPageStyle.css';
+
+function MainPage(){
+    return (
+        <div className='main-page'>
+            <div className="head-main">
+                <header >
+                    <div className="title-one">
+                        <div className="title-box-one title-box-one1">
+                            <a id="site-title" href="/" data-animation-role="header-element">StyloSphere</a>
+                        </div>
+
+
+                        <div className="title-box-one title-box-one2">
+                            <nav className="title-box-nav">
+                                <div className="title-box-nav-item title-box-nav-nPage">
+                                    <a href="/new-page" data-animation-role="header-element">
+                                        New Page
+                                    </a>
+                                </div>
+                                <div className="title-box-nav-item title-box-nav-shop">
+                                    <a href="/shop" data-animation-role="header-element">
+                                        Shop
+                                    </a>
+                                </div>
+                                <div className="title-box-nav-item title-box-nav-about">
+                                    <a href="/about" data-animation-role="header-element">
+                                        About
+                                    </a>
+                                </div>
+                                <div className="title-box-nav-item title-box-nav-cart">
+                                    <a href="/cart" data-animation-role="header-element">
+                                        Cart
+                                    </a>
+                                </div>
+                            </nav>
+                        </div >
+                    </div>
+                </header>
+            </div>
+            <div className='page-main-one'>
+                <div className='page-main-one-title'>
+                    <h1>Unlock Your Marketing Potential Today</h1>
+                </div>
+                <div className='page-main-one-img'>
+                    <img src={mainPagepng} />
+                    <p class="overlay-text">Welcome to your marketing journey
+                        where creativity meets strategy. We're
+                        here to help you navigate the ever-changing
+                        landscape, turning possibilities
+                        into powerful results.
+                        </p>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
