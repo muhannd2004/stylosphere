d6bb83aaa06c63355a8ece26e58f22cc6ede989c
Moamen <moamen.hesham.code@gmail.com>
2024-12-13 04:47:48 +0200

    .

---
+++ backend/src/main/java/com/Prototype/StyloSphere/StyloSphereApplication.java
@@ -0,0 +1,13 @@
+package com.Prototype.StyloSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyloSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyloSphereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Prototype/StyloSphere/StyloSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prototype.StyloSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyloSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
